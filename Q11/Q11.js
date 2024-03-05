d682fd06d83184b4322a336a4524d6c637eb4b50
Maria <161065742+MariaKhan10@users.noreply.github.com>
2024-03-06 04:32:01 +0500

    Commit

--- Q11/Q11.js
+++ Q11/Q11.js
@@ -1,7 +1,5 @@
 // Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 var names = ["Aqsa", "Namra", "Maria", "Naheed", "jannat"];
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
-console.log(names[4]);
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
