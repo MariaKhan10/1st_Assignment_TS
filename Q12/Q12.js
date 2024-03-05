2b7cc836e1a26d089488675152a7a1b4b8f83330
Maria <161065742+MariaKhan10@users.noreply.github.com>
2024-03-06 04:37:30 +0500

    first commit

--- Q12/Q12.js
+++ Q12/Q12.js
@@ -1,8 +1,14 @@
 // Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
 var names = ["Aqsa", "Namra", "Maria", "Naheed", "jannat"];
-var message = "Have a nice day";
-console.log("Hello", names[0] + " " + message);
-console.log("Hello", names[1] + " " + message);
-console.log("Hello", names[2] + " " + message);
-console.log("Hello", names[3] + " " + message);
-console.log("Hello", names[4] + " " + message);
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello ".concat(name_1, ",would you like to learn some Typescript Today?"));
+}
+// Alternate Way
+// let names : string [] = ["Aqsa", "Namra", "Maria", "Naheed", "jannat"];
+// let message : string = "Have a nice day";
+// console.log("Hello" ,names[0] + " " + message);
+// console.log("Hello" ,names[1] + " " + message);
+// console.log("Hello" ,names[2] + " " + message);
+// console.log("Hello" ,names[3] + " " + message);
+// console.log("Hello" ,names[4] + " " + message);
