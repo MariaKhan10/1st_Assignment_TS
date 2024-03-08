d063c5a1e4fa1e74ff66c2a65cc7b8df24af4327
Maria <161065742+MariaKhan10@users.noreply.github.com>
2024-03-08 21:38:26 +0500

    first commit

--- Q11/Q11.js
+++ Q11/Q11.js
@@ -1,5 +1,5 @@
 // Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-var names = ["Aqsa", "Namra", "Maria", "Naheed", "jannat"];
+var names = ["Aqsa", "Namra", "Maria", "Naheed", "Jannat"];
 for (var i = 0; i < names.length; i++) {
     console.log(names[i]);
 }
--- Q11/Q11.ts
+++ Q11/Q11.ts
@@ -1,7 +1,7 @@
 // Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 
 
-let names : string [] = ["Aqsa", "Namra", "Maria", "Naheed", "jannat"];
+let names : string [] = ["Aqsa", "Namra", "Maria", "Naheed", "Jannat"];
 
 for (let i= 0; i < names.length; i++){
     console.log(names[i]);
